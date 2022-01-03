15748874416db0371e94860c103eec97a118ed22
Al3xander123 <96358474+Al3xander123@users.noreply.github.com>
2022-01-03 15:37:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.onload;
 
+document.getElementsByTagName("div")[0].innerHTML=
+"Epale Gente. Como estan";
+/*
 document.getElementsByTagName("textarea")[0].onchange=
 function(){
 document.getElementsByTagName("div")[0].innerHTML=
 document.getElementsByTagName("textarea")[0].value;}
+*/
