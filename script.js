8ab34d8d82ebb213f977431a7289aad165964605
Al3xander123 <96358474+Al3xander123@users.noreply.github.com>
2022-01-03 16:00:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.onload;
 let texto=document.getElementsByTagName("div")[0];
 texto.innerHTML=
 "Epale Gente. Como eajshahshahshstan";
-/*
+
 document.getElementsByTagName("textarea")[0].onchange=
 function(){
 document.getElementsByTagName("div")[0].innerHTML=
 document.getElementsByTagName("textarea")[0].value;}
-*/
+
