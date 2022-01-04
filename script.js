6816b3ef4c9485caad070d4a074707e961033d67
Al3xander123 <96358474+Al3xander123@users.noreply.github.com>
2022-01-03 23:02:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.onload;
 
-document.getElementsByTagName("textarea")[0].onkeyup=()=>{
+let texa=document.getElementsByTagName("textarea")[0];
+texa.onkeyup=()=>{
 document.getElementsByTagName("div")[0].innerHTML=
 document.getElementsByTagName("textarea")[0].value;}
 
-document.getElementsByTagName("textarea")[1].onkeyup=()=>{
+let texb=document.getElementsByTagName("textarea")[1];
+texb.onkeyup=()=>{
 document.getElementsByTagName("style")[1].innerHTML=
 document.getElementsByTagName("textarea")[1].value;}
