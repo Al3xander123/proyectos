f7b6c7da618ef851e0247067af9675b53f322682
Al3xander123 <96358474+Al3xander123@users.noreply.github.com>
2022-01-04 23:07:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.onload;
 
-document.getElementsByTagName("textarea")[0].onkeyup=()=>{
+let div_principal=document.getElementsByTagName("textarea")[0];
+div_principal.onkeyup=()=>{
 document.getElementsByClassName("div_princial")[0].innerHTML=
 document.getElementsByTagName("textarea")[0].value;}
 
-document.getElementsByTagName("textarea")[1].onkeyup=()=>{
+let estilo_principal=document.getElementsByTagName("textarea")[1];
+estilo_principal.onkeyup=()=>{
 document.getElementsByTagName("style")[1].innerHTML=
 document.getElementsByTagName("textarea")[1].value;}
