0591c66fc2d9b1b95cc22c0ec99145adcf44a471
Al3xander123 <96358474+Al3xander123@users.noreply.github.com>
2022-01-04 23:24:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 document.onload;
 
-let div_principal=document.getElementsByTagName("textarea")[0];
+let texta=document.getElementsByTagName("textarea")[0];
+texta.addEventListener("keyup",miDiv);
+let textb=document.getElementsByTagName("textarea")[1];
+textb.addEventListener("keyup",miEstilo);
+let divp=document.getElementsByClassName("div_principal")[0];
+let estilo=document.getElementsByTagName("style")[1];
+
+
+function miDiv(){
+divp.innerHTML=texta;
+}
+
+function miEstilo(){
+estilo.innerHTML=textb;
+}
+/*
 div_principal.onkeyup=()=>{
 document.getElementsByClassName("div_princial")[0].innerHTML=
 document.getElementsByTagName("textarea")[0].value;}
@@ -9,3 +24,4 @@ let estilo_principal=document.getElementsByTagName("textarea")[1];
 estilo_principal.onkeyup=()=>{
 document.getElementsByTagName("style")[1].innerHTML=
 document.getElementsByTagName("textarea")[1].value;}
+*/
